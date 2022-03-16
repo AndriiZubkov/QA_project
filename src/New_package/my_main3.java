3497c264242dbb4b851fc3058f858c7c4ef9c685
AndriiZubkov <a.ye.zubkov@gmail.com>
2022-03-16 13:11:46 +0200

    chetnoye-nechetnoye

--- src/New_package/my_main3.java
+++ src/New_package/my_main3.java
@@ -2,7 +2,7 @@ package New_package;
 
 public class my_main3 {
     public static void main(String[] args) {
-        int n = (int)(Math.random()*900+100),a,b,c;
+        int n = (int)(Math.random()*899+100),a,b,c;
         a = n/100;
         b = (n%100)/10;
         c = n%10;
