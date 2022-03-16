f5841204be51a11b45c2b97e95f61d63f1666a32
AndriiZubkov <a.ye.zubkov@gmail.com>
2022-03-16 13:13:10 +0200

    chetnoye-nechetnoye

--- src/New_package/my_main3.java
+++ src/New_package/my_main3.java
@@ -2,10 +2,10 @@ package New_package;
 
 public class my_main3 {
     public static void main(String[] args) {
-        int n = (int)(Math.random()*899+100),a,b,c;
-        a = n/100;
-        b = (n%100)/10;
-        c = n%10;
+        int n = (int)(Math.random()*999);
+        int a = n/100;
+        int b = (n%100)/10;
+        int c = n%10;
         if(a>=b&&a>=c) {
             System.out.println("В числе "+n+" наибольшая цифра "+a);
         } else if(b>=a&&b>=c) {
