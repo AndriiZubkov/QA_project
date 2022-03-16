226943ed9b3365d70595922e590f5bd02e194556
AndriiZubkov <a.ye.zubkov@gmail.com>
2022-03-16 13:00:34 +0200

    chetnoye-nechetnoye

--- src/New_package/my_main.java
+++ src/New_package/my_main.java
@@ -2,8 +2,7 @@ package New_package;
 
 public class my_main {
     public static void main(String[] args) {
-        int n;
-        n = 5;
+        int n = 10;
         if(n%2==0) {
             System.out.println(n+" - Четное число");
         } else {
