94c3da1643f5855cb156669f9a87c6fabb1aefdd
AndriiZubkov <a.ye.zubkov@gmail.com>
2022-03-16 11:12:02 +0200

    chetnoye-nechetnoye

--- src/New_package/my_main.java
+++ src/New_package/my_main.java
@@ -1,2 +1,13 @@
-package New_package;public class my_main {
-}
+package New_package;
+
+public class my_main {
+    public static void main(String[] args) {
+        int n;
+        n = 5;
+        if(n%2==0) {
+            System.out.println(n+" - Четное число");
+        } else {
+            System.out.println(n+" - Нечетное число");
+        }
+    }
+    }
